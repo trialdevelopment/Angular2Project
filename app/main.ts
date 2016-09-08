bcefb2be235f68f08ff68c17754da2d197890ce8
Jay <jaynagda@yahoo.com>
2016-09-07 21:22:08 -0400

    Commit

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+
+import {AppComponent} from './app.component';
+
+bootstrap(<any>AppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
